6b2e69d67a72743ab926a94d356fab057a92e557
huanchin <cherry032826@gmail.com>
2024-02-05 17:54:56 +0800

    first commit

---
+++ streams.js
@@ -0,0 +1,42 @@
+// read large text file from file system and send it to the client
+// demostrate how to read large file by consuming streams
+
+const fs = require("fs");
+const server = require("http").createServer();
+
+server.on("request", (req, res) => {
+  // solution 1: file is too large! reading the whole file into a variable(data) and store it into memory can make the performance really bad
+  //   fs.readFile("test-file.txt", (err, data) => {
+  //     if (err) console.log(err);
+  //     res.end(data);
+  // });
+
+  // solution 2: Streams
+  // there is no need to read the data from file into a variable
+  // just create a readable stream
+  // this solution can cause back-pressure when readable stream is way more faster then sending the resualt with the response writtable stream
+  // (receiving speed >>> responsing speed)
+  //   const readable = fs.createReadStream("test-file.txt");
+  //   readable.on("data", (chunk) => {
+  //     // response is also a writtable stream
+  //     res.write(chunk);
+  //   });
+  //   readable.on("end", () => {
+  //     res.end();
+  //   });
+  //   readable.on("error", (err) => {
+  //     console.log(err);
+  //     res.statusCode = 500;
+  //     res.end("file not found");
+  //   });
+
+  // solution 3: using pipe operator
+  // pipe the output of readable stream right into the input of a writable stream
+  // readablSource.pipe(writableDest)
+  const readable = fs.createReadStream("test-file.txt");
+  readable.pipe(res);
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening...");
+});
